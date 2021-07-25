779ba6e5539d43bc94ff86a7c1598bab30e00522
Violeta Reimer <violeta_reimer98@hotmail.com>
2021-07-25 18:53:53 -0300

    menu desplegable

--- app/src/main/java/com/example/pharmapp/MainActivity.java
+++ app/src/main/java/com/example/pharmapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pharmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //metodo boton siguiente
+    public void Siguiente(View view) {
+        Intent siguiente = new Intent(this, Main2Activity.class);
+        startActivity(siguiente);
+
     }
 }
