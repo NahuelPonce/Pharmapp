4b469adf81289856f40e7a99c99df3c1c51dcba3
Violeta Reimer <violeta_reimer98@hotmail.com>
2021-10-22 11:29:33 -0300

    agrego nuevo nav

---
+++ app/src/main/java/com/example/pharmapp/ui/orden/OrdenFragment.java
@@ -0,0 +1,51 @@
+package com.example.pharmapp.ui.orden;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.pharmapp.R;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static androidx.navigation.Navigation.findNavController;
+
+public class OrdenFragment extends Fragment {
+
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_orden, container,false);
+
+
+
+
+        return v;
+    }
+
+
+
+}
